b792bd6d16417231242cc2bf6c56ed322c82c478
picatostao <valver123@gmail.com>
2023-05-05 11:36:46 +0200

    cambios

---
+++ poo/person.js
@@ -0,0 +1,30 @@
+class person{
+    constructor(name, surname, year, kg,cm)
+    {
+        this.nombre=name;
+        this.apellido=surname;
+        this.anyoNacimiento=year;
+        this.peso= kg;
+        this.altura= cm;
+        this.imc=this.calcImc();
+        this.edad=this.calcedad(2023);
+        this.hobbies=["geologia","acupuntura","tiro con arco"];
+    }
+    calcImc(){                
+        return this.peso/(this.altura*this.altura);
+    }
+    calcedad(){
+        return currentyear - this.anyoNacimiento ;   
+    }
+    printAll(){
+        return `${this.nombre}${"-"}${this.apellidos}${"-"}${this.anyoNacimiento}${"-"}${this.peso}${"-"}`
+    }
+    printHobbies(){
+        return this.hobbies.length
+    }
+}
+module.exports={person
+}
+let currentyear=2023
+let andres=new person("Andres","perez",1995,100,180,2023)
+console.log(andres)
---
+++ poo/personTest.js
@@ -0,0 +1,5 @@
+let myLib= require("./person")
+
+let currentyear=2023
+let andres=new person("Andres","perez",1995,100,180,2023)
+console.log(andres)
