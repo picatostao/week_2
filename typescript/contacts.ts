a007c76ba469e90d883cca3d2e36a754927d8258
picatostao <valver123@gmail.com>
2023-05-10 16:32:48 +0200

    fin

--- typescript/contacts.ts
+++ typescript/contacts.ts
@@ -2,14 +2,16 @@ import{Person}from"./person"
 let contacto:Person=new Person("Jaime",17,"calle calle 15")
 
 export class Contacts{
-    public pepole:object[];
+    public pepole:Person[];
     /////constructor///
     constructor(){
         this.pepole=[];
     }
     //////metodos////
     public printCalendar(){
-        
-        return this.pepole
+        for(let person of this.pepole)
+        console.log(`Nombre: ${person.name}
+        Edad:${person.age}
+        Direccion: ${person.getAdress()}`)
     }
 }
--- typescript/contacttest.ts
+++ typescript/contacttest.ts
@@ -5,4 +5,4 @@ let jose:Person=new Person("Jaimy",17,"calle calle 15")
 let josa:Person=new Person("Jaimu",17,"callo calle 15")
 let joso:Person=new Person("Jaimk",17,"calli calle 15")
 contactos.pepole=[jose,josa,joso]
-console.log(contactos.printCalendar())
+contactos.printCalendar()
